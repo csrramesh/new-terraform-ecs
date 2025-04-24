e7f0ecb01424bb4638e4d626574200e1a5602ec4
Ramesh <csramesh0@gmail.com>
2025-04-24 19:35:39 +0530

    2

---
+++ vpc.tf
@@ -0,0 +1,29 @@
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "drupal-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-southeast-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "drupal-public-subnet-a"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "drupal-igw"
+  }
+}
